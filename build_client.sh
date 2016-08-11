922703f5ab94e62ff36041ba304b31b87a263bf0
Jordan Bray <jordan@northwoodmachine.com>
2016-08-11 14:08:23 -0400

    I'll add these back, for now...

--- build_client.sh
+++ build_client.sh
@@ -3,15 +3,11 @@
 set -e
 
 #git reset --hard
-python build/replace_versions.py
+#python build/replace_versions.py
 
 if ! test -e build_client_ok
 then
 	./switch_build.sh client
-	wget https://www.cryptopp.com/cryptopp563.zip -O cryptoplugin/cryptopp563.zip
-	cd cryptoplugin
-	unzip cryptopp563.zip
-	cd ..
 	autoreconf || true
 	automake --add-missing || true
 	libtoolize || true
