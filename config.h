bac0e0f47162935ed7f33e75fa528111f7bd4f00
Jordan Bray <jordan@northwoodmachine.com>
2016-08-11 14:23:01 -0400

    Working clientctl.

---
+++ config.h
@@ -0,0 +1,86 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+/*
+#define HAVE_ARPA_INET_H 1
+
+#define HAVE_FALLOCATE64 1
+
+#define HAVE_FCNTL_H 1
+
+#define HAVE_GETTIMEOFDAY 1
+
+#define HAVE_INTTYPES_H 1
+
+#define HAVE_MEMORY_H 1
+
+#define HAVE_MEMSET 1
+
+#define HAVE_MNTENT_H 1
+
+#define HAVE_NETDB_H 1
+
+#define HAVE_NETINET_IN_H 1
+
+#define HAVE_PTHREAD_H 1
+
+#define HAVE_SELECT 1
+
+#define HAVE_SOCKET 1
+
+#define HAVE_SPAWN_H 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_STRFTIME 1
+
+#define HAVE_STRINGS_H 1
+
+#define HAVE_STRING_H 1
+
+#define HAVE_STRSTR 1
+
+#define HAVE_SYNCFS 1
+
+#define HAVE_SYS_SELECT_H 1
+
+#define HAVE_SYS_SOCKET_H 1
+
+#define HAVE_SYS_STAT_H 1
+
+#define HAVE_SYS_TIME_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_UNISTD_H 1
+*/
+#define PACKAGE "urbackup-client"
+
+#define PACKAGE_BUGREPORT "martin@urbackup.org"
+
+#define PACKAGE_NAME "urbackup-client"
+
+#define PACKAGE_STRING "urbackup-client 2.0.31.BUILDID"
+
+#define PACKAGE_TARNAME "urbackup-client"
+
+#define PACKAGE_URL ""
+
+#define PACKAGE_VERSION "2.0.31.BUILDID"
+/*
+#define SELECT_TYPE_ARG1 int
+
+#define SELECT_TYPE_ARG234 (fd_set *)
+
+#define SELECT_TYPE_ARG5 (struct timeval *)
+
+#define STDC_HEADERS 1
+
+#define TIME_WITH_SYS_TIME 1
+*/
+#define VERSION "2.0.31.BUILDID"
+
+#ifndef __cplusplus
+#endif
+
